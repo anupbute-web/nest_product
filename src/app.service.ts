269eb5720e2f0e1742f0aa98a3f626406a859621
Anup_Bute <anupbute7517355998@gmail.com>
2025-09-30 14:26:13 +0530

    2nd commit

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'Hello World! to view all products go to <a href="/products">products</a>';
   }
 }
